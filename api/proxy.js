708c2fc220f81c70cac04b7a5c2e48c61ff1af67
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2024-08-01 09:42:18 +0300

    change source and destination vercel.json

--- api/proxy.js
+++ api/proxy.js
@@ -1,39 +1,18 @@
-// const https = require('https');
+const https = require('https');
 
-// export default function handler(req, res) {
-//   const options = {
-//     hostname: 'https://live.devnimble.com',
-//     port: 443,
-//     path: req.url,
-//     method: req.method,
-//     headers: req.headers
-//   };
+export default function handler(req, res) {
+  const options = {
+    hostname: 'https://live.devnimble.com',
+    port: 443,
+    path: req.url,
+    method: req.method,
+    headers: req.headers
+  };
 
-//   const proxy = https.request(options, (proxyRes) => {
-//     res.writeHead(proxyRes.statusCode, proxyRes.headers);
-//     proxyRes.pipe(res, { end: true });
-//   });
+  const proxy = https.request(options, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res, { end: true });
+  });
 
-//   req.pipe(proxy, { end: true });
-// }
-
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const apiProxy = createProxyMiddleware({
-  target: 'https://live.devnimble.com',
-  changeOrigin: true,
-  pathRewrite: {
-    '': ''
-  },
-  path: req.url,
-  method: req.method,
-  headers: req.headers,
-  onProxyRes(proxyRes) {
-    proxyRes.headers['x-added'] = 'foobar';
-    delete proxyRes.headers['x-removed'];
-  }
-});
-
-export default function (req, res) {
-  return apiProxy(req, res);
+  req.pipe(proxy, { end: true });
 }
