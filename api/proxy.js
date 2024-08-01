79a08c611e8d333c51f70c6623855fe59eceabfd
Dmitriy Kislitsyn <dima.music1989@gmail.com>
2024-08-01 09:25:23 +0300

    change proxy ssettings

--- api/proxy.js
+++ api/proxy.js
@@ -1,18 +1,39 @@
-const https = require('https');
+// const https = require('https');
 
-export default function handler(req, res) {
-  const options = {
-    hostname: 'https://live.devnimble.com',
-    port: 443,
-    path: req.url,
-    method: req.method,
-    headers: req.headers
-  };
+// export default function handler(req, res) {
+//   const options = {
+//     hostname: 'https://live.devnimble.com',
+//     port: 443,
+//     path: req.url,
+//     method: req.method,
+//     headers: req.headers
+//   };
 
-  const proxy = https.request(options, (proxyRes) => {
-    res.writeHead(proxyRes.statusCode, proxyRes.headers);
-    proxyRes.pipe(res, { end: true });
-  });
+//   const proxy = https.request(options, (proxyRes) => {
+//     res.writeHead(proxyRes.statusCode, proxyRes.headers);
+//     proxyRes.pipe(res, { end: true });
+//   });
 
-  req.pipe(proxy, { end: true });
+//   req.pipe(proxy, { end: true });
+// }
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const apiProxy = createProxyMiddleware({
+  target: 'https://example.org',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': ''
+  },
+  path: req.url,
+  method: req.method,
+  headers: req.headers,
+  onProxyRes(proxyRes) {
+    proxyRes.headers['x-added'] = 'foobar';
+    delete proxyRes.headers['x-removed'];
+  }
+});
+
+export default function (req, res) {
+  return apiProxy(req, res);
 }
